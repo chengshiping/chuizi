b6554990f6948d57e2aed0625fa5193774ff64ba
chengshiping <2302259502@qq.com>
2020-07-03 14:48:24 +0800

    加入数据库文件

---
+++ 数据库.sql
@@ -0,0 +1,26 @@
+﻿# Host: localhost  (Version: 5.7.26)
+# Date: 2020-07-03 14:41:22
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(255) DEFAULT NULL,
+  `pass` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'13247746920','a123456798',NULL),(4,'13247746960','a123456789',NULL),(5,'13247743652','a123456789',NULL),(6,'13245678526','a123456789',NULL),(7,'13245645856','a123456789',NULL),(8,'13211113333','a123456789',NULL),(9,'13247746921','a123456789',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
